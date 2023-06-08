c3925aa06d2ee2bdaa1aad9f54800d860f1ecd75
EmanSaeed331 <36940678+EmanSaeed331@users.noreply.github.com>
2023-06-09 01:27:52 +0300

    add: Database connection

--- tracker-exercise/server/server.js
+++ tracker-exercise/server/server.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const mongoose = require('mongoose');
 
 const app = express();
-const PORT = 8000 || process.env.PORT;
+require('dotenv').config();
+const PORT = process.env.PORT || 8000;
 app.use(cors());
 app.use(bodyParser.json());
 
-
+// Mongoose Connection 
+// Atlas URI 
+mongoose.connect(process.env.ATLAS_URI , { useNewUrlParser: true });
+const connection = mongoose.connection;
+connection.once('open',() => { 
+    console.log("MONGO DATABASE connection established successfully ");
+})
 app.listen(PORT, ()=>{ 
     console.log(`server is up on Port ${PORT} 🚀`);
 })
